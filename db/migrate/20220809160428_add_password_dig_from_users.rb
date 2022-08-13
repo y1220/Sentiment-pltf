10d2b7da125cec84120919d20e3b5f1be0a9cccc
y1220 <ga.2.y1220@gmail.com>
2022-08-13 16:57:26 +0900

    change ruby version

--- db/migrate/20220809160428_add_password_dig_from_users.rb
+++ db/migrate/20220809160428_add_password_dig_from_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordDigFromUsers < ActiveRecord::Migration[6.1]
   def change
-    #add_column :users, :encrypted_password, :string
+    add_column :users, :encrypted_password, :string
   end
 end
