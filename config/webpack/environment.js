0ebca04ef5ae5192f1f438cdc111ca829219de53
y1220 <ga.2.y1220@gmail.com>
2022-08-14 00:13:15 +0900

    extract_css: false

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,3 @@
-const { environment } = require("@rails/webpacker");
+const { environment } = require('@rails/webpacker')
 
-const webpack = require("webpack");
-environment.plugins.prepend(
-  "Provide",
-  new webpack.ProvidePlugin({
-    // $: "jquery",
-    // jQuery: "jquery",
-    // Popper: "popper.js",
-    $: "jquery/src/jquery",
-    jQuery: "jquery/src/jquery",
-    Popper: ["popper.js", "default"],
-  })
-);
-
-module.exports = environment;
+module.exports = environment
